92617c4d3f7d535f74abb511bae5ba26a6312b08
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-03-01 09:54:36 +0100

    add not found page handling

---
+++ src/pages/404/NotFoundPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>404</h1>
+    <p>Ops! Url {{ notFound }} does not exists...</p>
+    <a @click="handleBackClicked">Back</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFoundPage",
+
+  computed: {
+    notFound() {
+      return this.$route.query.notFound;
+    },
+  },
+
+  methods: {
+    handleBackClicked() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -2,10 +2,26 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "@/pages/home/HomePage.vue";
 import AboutPage from "@/pages/about/AboutPage.vue";
+import NotFoundPage from "@/pages/404/NotFoundPage.vue";
 
 const routes = [
   { path: "/", component: HomePage },
   { path: "/about", component: AboutPage },
+  {
+    name: "NotFound",
+    path: "/:path(.*)*",
+    beforeEnter: (to) => {
+      return {
+        path: "/404",
+        replace: true,
+        query: { notFound: encodeURI(to.path) },
+      };
+    },
+  },
+  {
+    path: "/404",
+    component: NotFoundPage,
+  },
 ];
 
 const router = createRouter({
