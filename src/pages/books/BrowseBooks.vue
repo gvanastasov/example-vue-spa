cae97041a8b45ac671cd0a8b8e5bb842a3eab633
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-03-10 21:43:57 +0100

    add browse page

--- src/pages/books/BrowseBooks.vue
+++ src/pages/books/BrowseBooks.vue
@@ -1,9 +1,22 @@
 <template>
-  <div></div>
+  <b-container fluid>
+    <b-row>
+      <b-col cols="2">filters</b-col>
+      <b-col cols="10">
+        <c-product-grid></c-product-grid>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
+import { CProductGrid } from "@/components";
+
 export default {
   name: "BrowseBooks",
+
+  components: {
+    CProductGrid,
+  },
 };
 </script>
