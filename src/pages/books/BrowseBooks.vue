0bfdbc5b31126e1864cf65defcb33eee8f616b4a
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-03-10 22:28:24 +0100

    add heading for browse

--- src/pages/books/BrowseBooks.vue
+++ src/pages/books/BrowseBooks.vue
@@ -3,6 +3,11 @@
     <b-row>
       <b-col cols="2">filters</b-col>
       <b-col cols="10">
+        <l-section jumbo>
+          <template #header>Browse</template>
+          <template #lead> </template>
+          <p>{{ sections.main.content }}</p>
+        </l-section>
         <c-product-grid></c-product-grid>
       </b-col>
     </b-row>
@@ -10,13 +15,25 @@
 </template>
 
 <script>
-import { CProductGrid } from "@/components";
+import { LSection, CProductGrid } from "@/components";
+import { contentHelper } from "@/utils";
 
 export default {
   name: "BrowseBooks",
 
   components: {
+    LSection,
     CProductGrid,
+  },
+
+  data() {
+    return {
+      sections: {
+        main: {
+          content: contentHelper().generateSentences({ count: 1 }).build(),
+        },
+      },
+    };
   },
 };
 </script>
