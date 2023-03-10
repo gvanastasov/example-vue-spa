cd362dac24ed04d77f5e2b4508971e305ab67d82
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-03-10 14:34:55 +0100

    barrel components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
 // layout
+export { default as LCard } from "./layouts/LCard.vue";
+export { default as LPageDiaglog } from "./layouts/LPageDialog.vue";
+export { default as LSection } from "./layouts/LSection.vue";
+export { default as LSectionChain } from "./layouts/LSectionChain.vue";
 
 // components
 export { default as CProductCard } from "./products/CProductCard.vue";
