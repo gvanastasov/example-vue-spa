746ed38a8bbebb6e8309b6a7b68b8e3cb5ebbb91
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-03-10 10:28:58 +0100

    improve home page

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,9 +1,83 @@
 <template>
-  <div>home</div>
+  <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333"
+    >
+      <b-carousel-slide
+        v-for="s in slides"
+        :key="s.id"
+        :caption="s.title"
+        :img-alt="s.title"
+        img-blank
+      >
+        <p>{{ s.text }}</p>
+      </b-carousel-slide>
+    </b-carousel>
+    <b-container>
+      <l-section-chain>
+        <l-section jumbo>
+          <template #header>Example Book Store</template>
+          <template #lead>
+            This is a simple bookstore website, used to showcase how to build
+            single page applications (SPA) with vue.js (major version 3).
+          </template>
+          <p>{{ sections.main.content }}</p>
+        </l-section>
+
+        <l-section>
+          <p>Check our latest books</p>
+          <b-card-group deck>
+            <c-product-card></c-product-card>
+            <c-product-card></c-product-card>
+            <c-product-card></c-product-card>
+            <c-product-card></c-product-card>
+          </b-card-group>
+        </l-section>
+      </l-section-chain>
+    </b-container>
+  </div>
 </template>
 
 <script>
+import LSection from "@/components/layouts/LSection.vue";
+import LSectionChain from "@/components/layouts/LSectionChain.vue";
+import { CProductCard } from "@/components";
+import contentBuilder from "@/utils/content.js";
+
 export default {
   name: "HomePage",
+
+  components: {
+    LSectionChain,
+    LSection,
+    CProductCard,
+  },
+
+  data() {
+    return {
+      slide: 0,
+      slides: [...Array(10).keys()].map((x) => ({
+        id: x,
+        title: contentBuilder()
+          .generateWords({ min: 1, max: 3 })
+          .capitalizeFirstLetter()
+          .build(),
+        text: contentBuilder().generateSentences({ min: 1, max: 3 }).build(),
+      })),
+      sections: {
+        main: {
+          content: contentBuilder().generateSentences({ count: 2 }).build(),
+        },
+      },
+    };
+  },
 };
 </script>
