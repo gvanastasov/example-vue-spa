db66e24c910a6763e234cc038e7e236e5909e533
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-03-10 20:48:39 +0100

    fix i18n warning

--- webpack.config.js
+++ webpack.config.js
@@ -99,6 +99,9 @@ module.exports = {
     new webpack.DefinePlugin({
       __VUE_OPTIONS_API__: true,
       __VUE_PROD_DEVTOOLS__: false,
+      __VUE_I18N_FULL_INSTALL__: true,
+      __VUE_I18N_LEGACY_API__: true,
+      __INTLIFY_PROD_DEVTOOLS__: true,
     }),
     new WebpackBar(),
     new VueLoaderPlugin(),
