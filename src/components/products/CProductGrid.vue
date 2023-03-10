548e6379d3223a236e5cebb80be568454294873a
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2023-03-10 14:35:34 +0100

    render books grid

--- src/components/products/CProductGrid.vue
+++ src/components/products/CProductGrid.vue
@@ -1,11 +1,12 @@
 <template>
   <l-section>
-    <h3>Check our latest books</h3>
+    <h3 class="text-center mb-5">Check our latest books</h3>
     <b-card-group deck>
-      <c-product-card></c-product-card>
-      <c-product-card></c-product-card>
-      <c-product-card></c-product-card>
-      <c-product-card></c-product-card>
+      <c-product-card
+        v-for="book in books"
+        :key="book.id"
+        v-bind="book"
+      ></c-product-card>
     </b-card-group>
   </l-section>
 </template>
@@ -14,11 +15,26 @@
 import { mapActions } from "pinia";
 import { useBookStore } from "@/stores";
 
+import { LSection, CProductCard } from "@/components";
+
 export default {
   name: "CProductGrid",
 
+  components: {
+    LSection,
+    CProductCard,
+  },
+
+  data() {
+    return {
+      books: [],
+    };
+  },
+
   created() {
-    this.fetchBooks();
+    this.fetchBooks().then(({ books }) => {
+      this.books = books.slice(0, 4);
+    });
   },
 
   methods: {
